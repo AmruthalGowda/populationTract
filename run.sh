f9a5426b821fa603012451b7e627a05a0eb401cc
Amrutha Lakshmane Gowda <alakshmanegowda@hawk.iit.edu>
2020-10-18 17:49:33 -0500

    test2

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #
 # Use this shell script to compile (if necessary) your code and then execute it. Below is an example of what might be found in this file if your program was written in Python 3.7. Remember that in order to execute the following command, you need to uncomment the line
-python3.7 ./src/population.py ./input/censustract-00-10.csv ./output/report.csv
+python3.8.1 ./src/population.py ./input/censustract-00-10.csv ./output/report.csv
